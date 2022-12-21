6ac842fb4e9f08c4428f41e77e9f8c8c13ed4330
Pongja <58586037+pongja@users.noreply.github.com>
2022-12-21 23:12:51 +0900

    푸터수정

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -105,6 +105,14 @@
       </div>
     </div>
   </div>
+  <div class="footer">
+    <div class="footer_wrap">
+      <div class="footer_text">
+        <p>GitHub : https://github.com/pongja</p>
+        <p>email : rlqjadl2222@naver.com</p>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
 
@@ -129,5 +137,28 @@ Array.prototype.forEach.call(itemList, function(section){
 @import "~/assets/main.css";
 @import "~/assets/reset.css";
 
-
+.footer{
+  width: 100%;
+  height: 140px;
+  display: flex;
+  align-items: center;
+  background: #1B1B1E;
+  /* position: absolute;
+  bottom: 0; */
+ }
+ .footer .footer_wrap {
+  max-width: 1400px;
+  margin: 0 auto;
+  padding: 0 16px;
+}
+.footer .footer_text {
+  color: white;
+  text-align: center;
+  margin-top: 12px;
+}
+.footer .footer_text p {
+  line-height: 1.6;
+  font-size: 15px;
+  margin-bottom: 0;
+}
 </style>
