bbc1b7de6631fda6981a2a9392184fe7e46df202
pongja <58586037+pongja@users.noreply.github.com>
2022-12-22 12:18:35 +0900

    정보페이지 수정

--- src/components/ToItemPage.vue
+++ src/components/ToItemPage.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <h3 class="info_long">
+      롱빈터 정보
+    </h3>
     <div class="fish_item">
       <Carousel
         :wrap-around="true"
@@ -18,11 +21,17 @@
 
         <template #addons>
           <Navigation />
-          <Pagination />
         </template>
       </Carousel>
     </div>
-    <YouTube url="https://youtu.be/GQmO52f26Ws" />
+    <div class="youtube_container">
+      <h3 class="youtube_title">
+        공식영상
+      </h3>
+      <YouTube
+        src="https://youtu.be/GQmO52f26Ws" 
+        class="youtube" />
+    </div>
   </div>
   <div class="footer">
     <div class="footer_wrap">
@@ -35,8 +44,9 @@
 </template>
 <script setup>
 import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { Carousel, Slide,Navigation } from 'vue3-carousel'
 import YouTube from 'vue3-youtube'
+
 
 
 const slides = [
@@ -56,11 +66,6 @@ const slides = [
   justify-content: center;
   align-items: center;
 }
-
-.carousel__slide {
-  padding: 10px;
-}
-
 .carousel__prev,
 .carousel__next {
   box-sizing: content-box;
@@ -68,27 +73,45 @@ const slides = [
   background-color: white;
 
 }
+.youtube {
+  margin: 0 auto;
+}
+.youtube_title{
+  text-align: center;
+  font-size: 25px;
+}
+.youtube_container{
+  max-width: 960px;
+  margin: 0 auto;
+}
 .slideimg{
   width: 100%;
   height: 400px;
+  border-radius: 40px;
 }
 .container {
   max-width: 960px;
   margin: 0 auto;
   height: 100vh;
-  padding-top: 200px;
+  padding: 100px 16px 0px 16px;
 }
-
+.info_long{
+  font-weight: bold;
+  font-size: 27px;
+}
 /* footer */
 .footer {
   width: 100%;
   height: 140px;
+  margin-top: 100px;
   display: flex;
   align-items: center;
   background: #1B1B1E;
   /* position: absolute;
   bottom: 0; */
 }
+
+
 
 .footer .footer_wrap {
   max-width: 1400px;
@@ -106,5 +129,8 @@ const slides = [
   line-height: 1.6;
   font-size: 15px;
   margin-bottom: 0;
+}
+@media screen and (max-width:768px) {
+
 }
 </style>
