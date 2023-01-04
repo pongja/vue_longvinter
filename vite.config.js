4b95fdcbff4850bd1c51a2aa17ec6f19fbb956f5
pongja <58586037+pongja@users.noreply.github.com>
2023-01-04 17:29:24 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,14 @@ export default defineConfig({
       reactivityTransform: true,
     }),
   ],
-  server: {
-    proxy: {
-      '/': {
-        target: 'https://mockend.com/org/repo/posts',
-        changeOrigin: true,
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/Notice': {
+  //       target: 'https://mockend.com/org/repo/posts',
+  //       changeOrigin: true,
+  //     }
+  //   }
+  // },
   base: './',
   resolve: {
     alias: [{ find: '~', replacement: `${__dirname}/src` }],
